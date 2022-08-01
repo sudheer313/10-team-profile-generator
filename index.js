92e90c716ca745cbdfa971f45c2a0c8cea35955d
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-01 17:14:50 +1000

    updated test files

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer =  require("inquire");
+const fs = require("fs");
+
+// profiles
+const Manager = require("./lib/Manager");
+const Intern =  require("./lib/Intern");
+const Engineer = require('./lib/Engineer');
+
+// generate idex.html
+
+const generateHtml = require("./src/generateHtml");
+
