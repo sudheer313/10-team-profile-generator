8628d746afd5f20f9d11847ff7062178c5a2b4c1
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-05 18:19:45 +1000

    updated index.js

--- index.js
+++ index.js
@@ -7,46 +7,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const generateHtml = require("./src/generateHtml.js");
-
-const path = require ('path');
-
-
-const team_Members = [];
-
-const managerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Please enter your name?(*)",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "Please enter  employee ID(*)?",
-  },
-  {
-    type: "input",
-    name: "emailaddress",
-    message: "Please enter  email address(*)?",
-  },
-  {
-    type: "input",
-    name: "office",
-    message: "Please enter  office number(*)?",
-  },
-  {
-    type: "list",
-    name: "addEmployee",
-    message: "Who would you like to add next?",
-    choices: ['Engineer','Intern','Buid Team'],
-  }
-];
-
-const engineerQuestions = [
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "Please enter  name(*)?",
+      message: "Please enter your name?(*)",
     },
     {
       type: "input",
@@ -56,131 +22,62 @@ const engineerQuestions = [
     {
       type: "input",
       name: "emailaddress",
-      message: "Please enter your  address(*)?",
+      message: "Please enter  email address(*)?",
     },
     {
       type: "input",
-      name: "github",
-      message: "Please enter github username(*)?",
-    },
-    {
-      type: "list",
-      name: "addEmployee",
-      message: "Who would you like to add next(*)?",
-      choices: ['Engineer','Intern','Buid Team'],
+      name: "office",
+      message: "Please enter  office number(*)?",
     }
-  ];
-  
-const internQuestions = [
-    {
-      type: "input",
-      name: "name",
-      message: "Please enter your name(*)?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter your employee ID(*)?",
-    },
-    {
-      type: "input",
-      name: "emailaddress",
-      message: "Please enter your email address(*)?",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "Please enter your school name(*)?",
-    },
-    {
-        type: "list",
-        name: "addEmployee",
-        message: "Who would you like to add next?",
-        choices: ['Engineer','Intern','Buid Team'],
-      }
-  ];
-  
-function addManager() {
-    inquirer.prompt(managerQuestions).then((managerResponse) => {
-      console.log(managerResponse);
-      const manager = new Manager(
-          managerResponse.name,
-          managerResponse.id,
-          managerResponse.emailaddress,
-          managerResponse.office,
-      );
-      team_Members.push(manager);
-      switch (managerResponse.addEmployee){
-        case 'Engineer':
-            addEngineer();
-           break;
-        case  'Intern':
-            addIntern();
-            break;
-        default : 
-        return writeToFile(team_Members); 
-              
-      }
-    });
-  }
-function addEngineer() {
-  inquirer.prompt(engineerQuestions).then((engineerResponse) => {
-    console.log(engineerResponse);
-    const engineer = new Engineer(
-        engineerResponse.name,
-        engineerResponse.id,
-        engineerResponse.emailaddress,
-        engineerResponse.github
-    );
-    team_Members.push(engineer);
-    switch (engineerResponse.addEmployee){
-        case 'Engineer':
-           addEngineer();
-           break;
-        case  'Intern':
-            addIntern();
-            break;
-        default : 
-        return writeToFile(team_Members);       
-
-      }
-  });
-}
-function addIntern() {
-  inquirer.prompt(internQuestions).then((internResponse) => {
-    const intern = new Intern(
-        internResponse.name,
-        internResponse.id,
-        internResponse.emailaddress,
-        internResponse.school
-    );
-    team_Members.push(intern);
-    switch (internResponse.addEmployee){
-        case 'Engineer':
-           addEngineer();
-           break;
-        case  'Intern':
-            addIntern();
-            break;
-        default : 
-        return writeToFile(team_Members);    
-      }
-  });
-}
- addManager();
- 
-function writeToFile(data) {
-fs.writeFile('./dist/index.html', generateHtml(data),'utf-8',(err) =>{
-  if (err)
-  console.log(err);
-  else {
-      console.log('"Success! Your team profile has been generated"')
-  }
-})
+  ]);
 };
 
+// Bonus using writeFileSync as a promise
+const init = () => {
+  promptUser()
+  
+    // Use writeFileSync method to use promises instead of a callback function
+    .then((answers) => 
+    fs.writeFileSync('index.html', generateHTML(answers)))
+    
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+};
 
+init();
 
+const generateHTML = ({ name, id, emailaddress, office}) =>
+//console.log(name, location, github, linkedin);
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-dark justify-content-center align-items-center">
+<span class="navbar-brand mb-0 h1">
+      <h1>My Team Profile</h1>
+  </span>
+</nav>
+<div class ="card-container">
 
+<div class="card mb-3" style="max-width: 18rem;">
+  <div class="card-header"><h3>dgvs</h3><i class="fa-solid fa-mug-hot"></i> Manager
+  </div>
+  <div class="card-body text-dark">
+    <h5 class="card-title"></h5>
+    <p class="card-text"></p>
+    <ul class="list-group">
+      <li class="list-group-item">Company_Id: ${id}</li>
+      <li class="list-group-item">Email: <a href = "mailto:e-mail">${emailaddress}</a></li>
+      <li class="list-group-item">Office_number: ${office}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
 
 
