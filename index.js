5322fd5468bd5d1c384b1b7fbd52faffe9c0bd60
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-08-05 18:59:28 +1000

    pdated js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 const generateHtml = require("./src/generateHtml.js");
-const promptUser = () => {
+const Manager_questions = () => {
   return inquirer.prompt([
     {
       type: "input",
@@ -28,14 +28,70 @@ const promptUser = () => {
       type: "input",
       name: "office",
       message: "Please enter  office number(*)?",
+    },
+    {
+      type: "list",
+      name: "addEmployee",
+      message: "Who would you like to add next?",
+      choices: ['Engineer','Intern','Buid Team'],
     }
   ]);
 };
+const Engineer_questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter your name?(*)",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter  employee ID(*)?",
+    },
+    {
+      type: "input",
+      name: "emailaddress",
+      message: "Please enter  email address(*)?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter github username(*)?",
+    },
+  ]);
+};
 
+const intern_questions = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Please enter your name?(*)",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Please enter  employee ID(*)?",
+    },
+    {
+      type: "input",
+      name: "emailaddress",
+      message: "Please enter  email address(*)?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "Please enter your school name(*)?",
+    },
+   
+  ]);
+};
 // Bonus using writeFileSync as a promise
 const init = () => {
-  promptUser()
-  
+  Manager_questions()
+  //Engineer_questions()
+  //intern_questions()
     // Use writeFileSync method to use promises instead of a callback function
     .then((answers) => 
     fs.writeFileSync('index.html', generateHTML(answers)))
@@ -54,7 +110,7 @@ const generateHTML = ({ name, id, emailaddress, office}) =>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <title>Team Profile Generator</title>
 </head>
 <body>
 <nav class="navbar navbar-dark justify-content-center align-items-center">
@@ -68,7 +124,7 @@ const generateHTML = ({ name, id, emailaddress, office}) =>
   <div class="card-header"><h3>dgvs</h3><i class="fa-solid fa-mug-hot"></i> Manager
   </div>
   <div class="card-body text-dark">
-    <h5 class="card-title"></h5>
+    <h5 class="card-title">${name}</h5>
     <p class="card-text"></p>
     <ul class="list-group">
       <li class="list-group-item">Company_Id: ${id}</li>
@@ -80,4 +136,4 @@ const generateHTML = ({ name, id, emailaddress, office}) =>
 </body>
 </html>`;
 
-
+//referred from https://github.com/ashachakre0906 bootstrap module
